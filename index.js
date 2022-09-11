74041aa77f111eb36e74a1468780116ce1b4ad16
rjarovoj@web-bee.ru <useit14@inbox.ru>
2022-09-11 14:22:49 +0300

    Fix problems

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const sendData = (url, method, data) =>
     headers: {
       "Content-type": "application/json;charset=UTF-8",
     },
-  }).then((respone) => respone.json());
-const getData = (url) => fetch(url).then((respone) => respone.json());
+  })
+    .then((respone) => respone.json())
+    .catch((error) => alert(`Error: ${error}`));
+const getData = (url) => {
+  fetch(url)
+    .then((data) => {
+      sendData("https://jsonplaceholder.typicode.com/posts", "POST", data);
+    })
+    .catch((error) => alert(`Error: ${error}`));
+};
 
-getData("db.json")
-  .then((data) =>
-    sendData("https://jsonplaceholder.typicode.com/posts", "POST", data).catch(
-      (error) => alert(`Error: ${error}`)
-    )
-  )
-  .catch((error) => alert(`Error: ${error}`));
+getData("db.json");
